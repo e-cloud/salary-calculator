204bf215dd8d0688dfa31d56f3204987cbf8d50c
e-cloud <saintscott119@gmail.com>
2022-04-14 15:05:09 +0800

    chore(ng-calculator): improve eslint settings

--- packages/ng-calculator/.eslintrc.js
+++ packages/ng-calculator/.eslintrc.js
@@ -1,43 +1,63 @@
 module.exports = {
-  "extends": "../../.eslintrc.json",
-  "ignorePatterns": ["!**/*", "dist"],
-  "overrides": [
+  root: true,
+  ignorePatterns: ["dist"],
+  overrides: [
     {
-      "files": ["*.ts"],
-      "parserOptions": {
-        "tsconfigRootDir": __dirname,
-        "project": ["./tsconfig.app.json", "./tsconfig.spec.json"],
-        "createDefaultProgram": true
+      files: ["*.ts"],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.app.json", "./tsconfig.spec.json"],
+        createDefaultProgram: true,
       },
-      "rules": {
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            endOfLine: "auto",
+          },
+        ],
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports-ts": "error",
+        "unused-imports/no-unused-imports": "error",
         "@angular-eslint/component-selector": [
           "error",
           {
-            "type": "element",
-            "prefix": "app",
-            "style": "kebab-case"
-          }
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
         ],
         "@angular-eslint/directive-selector": [
           "error",
           {
-            "type": "attribute",
-            "prefix": "app",
-            "style": "camelCase"
-          }
-        ]
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
       },
-      "extends": [
+      plugins: ["unused-imports"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
         "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates"
-      ]
+        "plugin:@angular-eslint/template/process-inline-templates",
+      ],
     },
     {
-      "files": [
-        "*.html"
+      files: ["*.html"],
+      extends: [
+        "plugin:prettier/recommended",
+        "plugin:@angular-eslint/template/recommended",
       ],
-      "extends": ["plugin:@angular-eslint/template/recommended"],
-      "rules": {}
-    }
-  ]
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            endOfLine: "auto",
+          },
+        ],
+      },
+    },
+  ],
 };
