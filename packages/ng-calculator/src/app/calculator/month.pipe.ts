8f5581011272f57f46254b33baa8479e091b800c
e-cloud <saintscott119@gmail.com>
2024-07-03 14:02:09 +0800

    feat: upgrade angular material 15

--- packages/ng-calculator/src/app/calculator/month.pipe.ts
+++ packages/ng-calculator/src/app/calculator/month.pipe.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line unused-imports/no-unused-imports-ts, unused-imports/no-unused-imports
 import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
@@ -5,7 +6,12 @@ import { DatePipe } from '@angular/common';
   name: 'month',
 })
 export class MonthPipe extends DatePipe implements PipeTransform {
-  transform(value: any, format?: any, timezone?: any, locale?: any): any {
+  override transform(
+    value: any,
+    format?: any,
+    timezone?: any,
+    locale?: any
+  ): any {
     const date = new Date();
     date.setMonth(value);
     return super.transform(date, 'MMMM');
